3f14de3be713c57d15b44ee9e2abf431fd40a5d9
Jayesha Malhotra <102062100+Jayeshaa@users.noreply.github.com>
2024-07-22 23:24:33 +0530

    Add files via upload

---
+++ main_lpd.py
@@ -0,0 +1,77 @@
+#import the dependencies
+import cv2
+import numpy as np
+from ultralytics import YOLO
+from sort.sort import *
+import util
+from util import get_car, read_license_plate, write_csv
+
+results = {}
+mot_tracker = Sort()
+
+# Load the model
+model = YOLO('yolov8n.pt')
+
+lpd = YOLO('license_plate_detector.pt')
+
+# Load the video
+cap = cv2.VideoCapture('sample.mp4')
+
+vehicles = [2, 3, 5, 7]  # Classes for car, bus, truck, etc.
+# read frames
+frame_num = -1
+ret = True
+while ret:
+    frame_num += 1
+    ret, frame = cap.read()
+    if ret and frame_num < 10:
+        results[frame_num] = {}
+        # Detect vehicles
+        detections = model(frame)[0]
+        detections_bb = []
+        for detection in detections.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = detection
+            if int(class_id) in vehicles:
+                detections_bb.append([x1, y1, x2, y2, score])
+
+        # track vehicles
+        track_ids = mot_tracker.update(np.asarray(detections_bb))
+
+        # Detect license plates
+        license_plates = lpd(frame)[0]
+
+        for license_plate in license_plates.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = license_plate
+
+            # Assign license plate to car
+            xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+
+            # Crop license plate
+            license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
+
+            # Process license plate number
+            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+            _, license_plate_crop_threshold = cv2.threshold(license_plate_crop_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+            # cv2.imshow('original_crop', license_plate_crop)
+            # cv2.imshow('threshold', license_plate_crop_threshold)
+
+            # cv2.waitKey(0)
+
+            # Recognize license plate number
+
+            license_plate_text, license_plate_text_score = util.read_license_plate(license_plate_crop_threshold)
+
+            if license_plate_text is not None:
+                results[frame_num][car_id] = {'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]}, 
+                                              'license_plate':{'bbox': [x1, y1, x2, y2],
+                                                               'text': license_plate_text,
+                                                               'bbox_score': score ,
+                                                               'text_score': license_plate_text_score}}
+# results
+
+write_csv(results, 'test.csv')
+ 
+
+
+            
